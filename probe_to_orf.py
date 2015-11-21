d6ddd64a7659da4947998ffbc7e9279ed3c8ea4f
Jiashun Zheng <jiashun.zheng@gmail.com>
2015-11-21 10:22:57 -0800

    something after checkout

--- probe_to_orf.py
+++ probe_to_orf.py
@@ -2,9 +2,6 @@
 import sys
 import re
 
-"""Convert probe id to orf"""
-
-
 #store probe_info from GPLXX file
 probe_info = {}
 pattern1 = re.compile('.*platform_table_begin')
@@ -31,21 +28,15 @@ with open(sys.argv[1]) as gpl:
 
 pattern3 = re.compile('.*series_matrix_table_begin')
 pattern4 = re.compile('.*series_matrix_table_end')
-title_pattern = re.compile('.*Sample_title')
 with open(sys.argv[2])as matrix:
     line = matrix.readline()
     head = None
     in_data = False
     while line:
-        title_match = title_pattern.match(line)
-        if title_match:
-            title = re.sub('\"','',line)
-            title = re.sub('^\!','',title)
-            print title.strip()
         match = pattern3.match(line)
         if match:
             head = matrix.readline()
-            #print head.strip()
+            print head.strip()
             in_data = True
         else:
             match = pattern4.match(line)
@@ -57,4 +48,3 @@ with open(sys.argv[2])as matrix:
                 if probe_id in probe_info and probe_info[probe_id][11]:
                     print probe_info[probe_id][11]+"\t"+"\t".join(items[1:])
         line = matrix.readline()
-
